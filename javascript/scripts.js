70be4cd67f4c6fa195b71c0e4789bcdebc2a37ce
Janet <janetkarpenske@gmail.com>
2020-06-03 13:50:28 -0700

    Added user interface to calculator project

---
+++ javascript/scripts.js
@@ -0,0 +1,26 @@
+const add = function(number1, number2) {
+    return number1 + number2;
+  };
+  
+  const subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+  
+  const multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+  
+  const divide = function(number1, number2) {
+    return number1 / number2;
+  };
+
+
+  $(document).ready(function() {
+    $("form#add").submit(function(event) {
+      event.preventDefault();
+      const number1 = parseInt($("#add1").val());
+      const number2 = parseInt($("#add2").val());
+      const result = add(number1, number2);
+      $("#output").text(result);
+    });
+  });
